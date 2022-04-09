2bce257e1affd53bb7822d9944805f0726ebdc2e
Lionel Sanchez <58988565+snLionel90@users.noreply.github.com>
2022-04-09 21:27:11 +0200

    Add files via upload

---
+++ YandereSimLauncher/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace YandereSimLauncher
+{
+    partial class YandereLauncher
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(YandereLauncher));
+            this.buttonDownload = new System.Windows.Forms.Button();
+            this.btnDeviantart = new System.Windows.Forms.Button();
+            this.btnGitHub = new System.Windows.Forms.Button();
+            this.btnYoutube = new System.Windows.Forms.Button();
+            this.btnInstagram = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.opcionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cerrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informacionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonDownload
+            // 
+            resources.ApplyResources(this.buttonDownload, "buttonDownload");
+            this.buttonDownload.Name = "buttonDownload";
+            this.buttonDownload.UseVisualStyleBackColor = true;
+            this.buttonDownload.Click += new System.EventHandler(this.buttonDownload_Click);
+            // 
+            // btnDeviantart
+            // 
+            resources.ApplyResources(this.btnDeviantart, "btnDeviantart");
+            this.btnDeviantart.Image = global::YandereSimLauncher.Resource1.icon_deviantart;
+            this.btnDeviantart.Name = "btnDeviantart";
+            this.btnDeviantart.UseVisualStyleBackColor = true;
+            this.btnDeviantart.Click += new System.EventHandler(this.btnDeviantart_Click);
+            // 
+            // btnGitHub
+            // 
+            resources.ApplyResources(this.btnGitHub, "btnGitHub");
+            this.btnGitHub.BackColor = System.Drawing.Color.Transparent;
+            this.btnGitHub.Image = global::YandereSimLauncher.Resource1.icon_github;
+            this.btnGitHub.Name = "btnGitHub";
+            this.btnGitHub.UseVisualStyleBackColor = false;
+            this.btnGitHub.Click += new System.EventHandler(this.btnGitHub_Click);
+            // 
+            // btnYoutube
+            // 
+            resources.ApplyResources(this.btnYoutube, "btnYoutube");
+            this.btnYoutube.Name = "btnYoutube";
+            this.btnYoutube.UseVisualStyleBackColor = true;
+            this.btnYoutube.Click += new System.EventHandler(this.btnYoutube_Click);
+            // 
+            // btnInstagram
+            // 
+            resources.ApplyResources(this.btnInstagram, "btnInstagram");
+            this.btnInstagram.Image = global::YandereSimLauncher.Resource1.icon_instagram;
+            this.btnInstagram.Name = "btnInstagram";
+            this.btnInstagram.UseVisualStyleBackColor = true;
+            this.btnInstagram.Click += new System.EventHandler(this.btnInstagram_Click);
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.Name = "progressBar1";
+            // 
+            // menuStrip1
+            // 
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.opcionesToolStripMenuItem,
+            this.modeToolStripMenuItem});
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // opcionesToolStripMenuItem
+            // 
+            resources.ApplyResources(this.opcionesToolStripMenuItem, "opcionesToolStripMenuItem");
+            this.opcionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.cerrarToolStripMenuItem,
+            this.informacionToolStripMenuItem});
+            this.opcionesToolStripMenuItem.Name = "opcionesToolStripMenuItem";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            resources.ApplyResources(this.aboutToolStripMenuItem, "aboutToolStripMenuItem");
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            // 
+            // cerrarToolStripMenuItem
+            // 
+            resources.ApplyResources(this.cerrarToolStripMenuItem, "cerrarToolStripMenuItem");
+            this.cerrarToolStripMenuItem.Name = "cerrarToolStripMenuItem";
+            // 
+            // informacionToolStripMenuItem
+            // 
+            resources.ApplyResources(this.informacionToolStripMenuItem, "informacionToolStripMenuItem");
+            this.informacionToolStripMenuItem.Name = "informacionToolStripMenuItem";
+            // 
+            // modeToolStripMenuItem
+            // 
+            resources.ApplyResources(this.modeToolStripMenuItem, "modeToolStripMenuItem");
+            this.modeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3});
+            this.modeToolStripMenuItem.Name = "modeToolStripMenuItem";
+            // 
+            // toolStripMenuItem2
+            // 
+            resources.ApplyResources(this.toolStripMenuItem2, "toolStripMenuItem2");
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            // 
+            // toolStripMenuItem3
+            // 
+            resources.ApplyResources(this.toolStripMenuItem3, "toolStripMenuItem3");
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // YandereLauncher
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnInstagram);
+            this.Controls.Add(this.btnYoutube);
+            this.Controls.Add(this.btnDeviantart);
+            this.Controls.Add(this.btnGitHub);
+            this.Controls.Add(this.buttonDownload);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "YandereLauncher";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDownload;
+        private System.Windows.Forms.Button btnDeviantart;
+        private System.Windows.Forms.Button btnGitHub;
+        private System.Windows.Forms.Button btnYoutube;
+        private System.Windows.Forms.Button btnInstagram;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem opcionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cerrarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informacionToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem modeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+    }
+}
+
---
+++ YandereSimLauncher/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using System.Net;
+using System.IO;
+
+namespace YandereSimLauncher
+{
+    public partial class YandereLauncher : Form
+    {
+        //
+
+
+        public YandereLauncher()
+        {
+            InitializeComponent();
+            OnLoadImage();
+            //networkChecker = new NewtworkChecker
+        }
+
+        private void OnLoadImage()
+        {
+            
+        }
+
+
+        private void btnGitHub_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                VisitLink();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No disponile");
+            }
+        }
+
+        private void VisitLink()
+        {
+            System.Diagnostics.Process.Start("https://github.com/snlionel90");
+            //throw new NotImplementedException();
+        }
+
+        private void btnDeviantart_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnYoutube_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnInstagram_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonDownload_Click(object sender, EventArgs e)
+        {
+            //Sincrome
+            //WebClient wc= new WebClient();
+            //wc.DownloadFile("", "");
+            //MessageBox.Show("Donwloading Competed");
+
+            //Asincrine
+            if (!File.Exists("")) {
+                WebClient wc = new WebClient();
+                wc.DownloadFileCompleted += new AsyncCompletedEventHandler(Completo);
+                wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                wc.DownloadFileAsync(new Uri(""), "");
+            }   
+        }
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e) 
+        {
+
+        }
+        private void Completo(object sender, AsyncCompletedEventArgs e) 
+        {
+
+        }
+    }
+}
+
